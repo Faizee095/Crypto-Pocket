2199011f053afe348cf39fa5f87b76732fff9111
Faizee095 <faiyazsiddique9@gmail.com>
2023-02-09 16:24:03 +0530

    News Section Added

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+  'x-bingapis-sdk': 'true',
+  'x-rapidapi-key': "c03b2b8081mshc3d6c7fca60178dp1f77f3jsnc6e0695beef9",
+  'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl:"https://bing-news-search1.p.rapidapi.com/news" }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
