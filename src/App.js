e55a4d8c390cf3c7386d1d83bc2380ce24c385cd
Faizee095 <faiyazsiddique9@gmail.com>
2023-02-09 12:41:06 +0530

    Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  CryptoCurrencies,
+  CryptoDetails,
+  News,
+  Exchanges,
+  Homepage,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<CryptoCurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Crypto-Pocket All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/cryptocurrencies">CryptoCurrencies</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
